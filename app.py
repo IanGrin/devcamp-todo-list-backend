b1dae77ae549e90d6d6b7c079f6edd808d96ee55
Ian Merritt <iangrin.coding@gmail.com>
2023-07-25 11:29:16 -0600

    adjusted app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 
-import os
+# import os
 
 app = Flask(__name__)
 # basedir = os.path.abspath(os.path.dirname(__file__))
