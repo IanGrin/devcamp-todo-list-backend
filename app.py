c90c2c6117882aa3c9cbad8b1ee75a814c90541f
Ian Merritt <iangrin.coding@gmail.com>
2023-07-26 21:26:21 -0600

    heroku adjustment

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + \
 #     os.path.join(basedir, 'app.sqlite')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://fnzergzxycybba:465d500d8cc9b1afae5932d306add414d2a3e225cb3ee905373f1c4fdac377c5@ec2-44-206-204-65.compute-1.amazonaws.com:5432/d48v7f91vkks0g"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://fnzergzxycybba:465d500d8cc9b1afae5932d306add414d2a3e225cb3ee905373f1c4fdac377c5@ec2-44-206-204-65.compute-1.amazonaws.com:5432/d48v7f91vkks0g"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
