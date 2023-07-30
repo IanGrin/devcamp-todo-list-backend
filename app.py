fa8d2e59746f63761e105e3ee61569beace838bf
Ian Merritt <iangrin.coding@gmail.com>
2023-07-30 12:18:13 -0600

    minor adjustments

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+
 # basedir = os.path.abspath(os.path.dirname(__file__))
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + \
 #     os.path.join(basedir, 'app.sqlite')
